aa9c31c5e483cd2080471b9a8a79a79d31fad62c
Cibele Russo <cibelerusso@gmail.com>
2024-06-11 22:13:37 -0300

    Update ANOVA_fatorial_blocos.R

--- CodigosemR/ANOVA_fatorial_blocos.R
+++ CodigosemR/ANOVA_fatorial_blocos.R
@@ -6,6 +6,7 @@
 
 # Entrada de dados
 dados <- read.csv('https://raw.githubusercontent.com/cibelerusso/PlanejamentodeExperimentos/main/Dados/Intensidade_deteccao.csv')
+#View(dados)
 
 #install.packages('nlme')
 library(nlme)
@@ -21,15 +22,28 @@ dados$Operador <- factor(dados$Operador)
 boxplot(Intensidade.na.detecção ~ Filtro, data = dados, main = "Intensidade na Detecção por Tipo de Filtro")
 boxplot(Intensidade.na.detecção ~ Desordem.no.solo, data = dados, main = "Intensidade na Detecção por Desordem no Solo")
 
+# Visualização dos dados em painel
+#install.packages('Hmisc')
+#library('Hmisc')
 
-# Modelo fatorial
+attach(dados)
 
+library('lattice')
+xyplot(Intensidade.na.detecção ~ Filtro | Operador, pch=16)
+xyplot(Intensidade.na.detecção ~ Desordem.no.solo | Operador, pch=16)
+xyplot(Intensidade.na.detecção ~ Filtro + Desordem.no.solo | Operador, pch=16)
+
+
+# Modelo fatorial com blocos
 mod <- lme(fixed = Intensidade.na.detecção ~ Filtro * Desordem.no.solo, random = ~ 1 | Operador, data = dados)
 summary(mod)
+
+res<-residuals(mod)
 
 # Resíduos vs Valores Ajustados
 plot(fitted(mod), residuals(mod), pch=19, col="blue", main="Resíduos vs Ajustados")
 abline(h=0, col="red")
+
 
 # QQ-Plot
 qqnorm(res, col="blue", pch=19, main="QQ-Plot dos Resíduos")
@@ -65,21 +79,25 @@ dados$Operador <- factor(dados$Operador)
 dados$Dia <- factor(dados$Dia)
 
 
-
 # Visualização dos dados
 boxplot(Intensidade.na.detecção ~ Tipo.de.filtro, data = dados, main = "Intensidade na Detecção por Tipo de Filtro")
 boxplot(Intensidade.na.detecção ~ Desordem.no.solo, data = dados, main = "Intensidade na Detecção por Desordem no Solo")
-
+boxplot(Intensidade.na.detecção ~ Tipo.de.filtro + Desordem.no.solo, data = dados, main = "Intensidade na Detecção por Desordem no Solo")
 
 
 # Modelo fatorial
 
-mod <- lme(fixed = Intensidade.na.detecção ~ Tipo.de.filtro * Desordem.no.solo, random = ~ 1 | Operador/Dia, data = dados)
-summary(mod)
+mod1 <- lme(fixed = Intensidade.na.detecção ~ Tipo.de.filtro * Desordem.no.solo, random = ~ 1 | Operador/Dia, data = dados)
+summary(mod1)
+
+mod2 <- lme(fixed = Intensidade.na.detecção ~ Tipo.de.filtro * Desordem.no.solo, random = ~ 1 | Dia/ Operador, data = dados)
+summary(mod2)
+
+res <- residuals(mod1)
 
 
 # Resíduos vs Valores Ajustados
-plot(fitted(mod), residuals(mod), pch=19, col="blue", main="Resíduos vs Ajustados")
+plot(fitted(mod1), residuals(mod1), pch=19, col="blue", main="Resíduos vs Ajustados")
 abline(h=0, col="red")
 
 # QQ-Plot
@@ -87,14 +105,27 @@ qqnorm(res, col="blue", pch=19, main="QQ-Plot dos Resíduos")
 qqline(res)
 
 
+
 # Testes
 
 #### Normalidade dos erros
 
-shapiro.test(residuals(mod))
+shapiro.test(residuals(mod1))
 
 
 # Tabela ANOVA
-anova(mod)
+anova(mod1)
+
+mod1$groups
+
+mod1$modelStruct
+
+mod1$varFix
+
+mod1$sigma
+
+mod1$terms
+
+mod1$contrasts
 
 
